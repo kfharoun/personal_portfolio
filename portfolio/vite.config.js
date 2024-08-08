024134491e0da51d5c576411f8453974a3390ddb
Kass 🌸 <kassandra.ferland@gmail.com>
2024-08-08 11:39:11 -0400

    k

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-bootstrap'],
+      external: ['react-bootstrap/Modal'],
     },
   },
 });
